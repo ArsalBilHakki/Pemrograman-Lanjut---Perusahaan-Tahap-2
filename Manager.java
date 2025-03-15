f4a0f6cd38140a1f446d66c455c341965cb871f4
Arsal Bil Hakki R. Said <arsalbilhakki@gmail.com>
2025-03-15 22:17:27 +0700

    Add files via upload

---
+++ Manager.java
@@ -0,0 +1,13 @@
+public class Manager extends Karyawan {
+    private double insentif;
+
+    public Manager(String nama, double gajiPokok, Perusahaan perusahaan, double insentif) {
+        super(nama, gajiPokok, perusahaan);
+        this.insentif = insentif;
+    }
+
+    @Override
+    public double hitungGaji() {
+        return getGajiPokok() + insentif;
+    }
+}
